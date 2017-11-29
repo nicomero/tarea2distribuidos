ee0df590ef9e23492f4677e8215b303cc7bd6c2b
Nicomero <nicolas.alarcon.14@sansano.usm.cl>
2017-11-29 10:56:31 -0300

    hecho serializable

--- token.java
+++ token.java
@@ -1,10 +1,11 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.io.Serializable;
 
-public class token{
+public class token implements Serializable{
 
 
     int[] arrLN; //request mas exitoso reciente del proceso j en que el token se dio
-    Queue<Integer> colaQ=new LinkedList(); //IDs de procesos que esprean el token
+    Queue<Integer> colaQ; //IDs de procesos que esprean el token
 
 }
